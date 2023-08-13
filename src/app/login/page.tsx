57d6baba7e7a310a652ba1ff258fdbc55304d790
IconicLuck <uhasib959@gmail.com>
2023-08-13 20:54:45 +0600

    created a state to store user information

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,12 @@
-export default function LoginPage () {
+import React from "react"
+
+export default function LoginPage() {
+
+    const [user, setUser] = React.useState({
+        email: '',
+        password: ''
+    })
+
     return <div>
         <h1 className="text-center text-4xl">This is Login Page</h1>
     </div>
