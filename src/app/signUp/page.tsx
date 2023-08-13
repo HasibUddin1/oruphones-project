af56b2ba46c9d03086677ed18d14eacf64916682
IconicLuck <uhasib959@gmail.com>
2023-08-14 04:12:57 +0600

    added some changes for switching from login to signup

--- src/app/signUp/page.tsx
+++ src/app/signUp/page.tsx
@@ -36,7 +36,7 @@ export default function SignUpPage() {
                     <label htmlFor="floating_password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
                 </div>
                 <div className="w-fit hover:underline mb-5">
-                    <Link href='/signUp'><p>New to this site?</p></Link>
+                    <Link href='/login'><p>Already have an account?</p></Link>
                 </div>
                 <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
             </form>
