8bb2d24c4e24268f8f1d9cd48a538e01c545885e
IconicLuck <uhasib959@gmail.com>
2023-08-14 02:21:32 +0600

    created login and sign up page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import React from "react"
+import Link from "next/link"
 
 export default function LoginPage() {
 
@@ -7,7 +10,30 @@ export default function LoginPage() {
         password: ''
     })
 
-    return <div>
-        <h1 className="text-center text-4xl">This is Login Page</h1>
-    </div>
+    return (
+
+        <div className="w-9/12 mx-auto mt-20">
+            <form>
+                <div className="relative z-0 w-full mb-6 group">
+                    <input
+                        value={user.email}
+                        onChange={(event) => setUser({ ...user, email: event.target.value })}
+                        type="email" name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
+                </div>
+                <div className="relative z-0 w-full mb-6 group">
+                    <input
+                        value={user.password}
+                        onChange={(event) => setUser({ ...user, password: event.target.value })}
+                        type="password" name="floating_password" id="floating_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <label htmlFor="floating_password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
+                </div>
+                <div className="w-fit hover:underline mb-5">
+                    <Link href='/signUp'><p>New to this site?</p></Link>
+                </div>
+                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+            </form>
+        </div>
+
+    )
 }
--- src/app/signUp/page.tsx
+++ src/app/signUp/page.tsx
@@ -1,5 +1,45 @@
+'use client'
+
+import Link from "next/link";
+import React from "react";
+
 export default function SignUpPage() {
-    return <div>
-        <h1 className="text-4xl text-center">This is Sign Up Page</h1>
-    </div>
+
+    const [user, setUser] = React.useState({
+        name: '',
+        email: '',
+        password: ''
+    })
+
+    return (
+        <div className="w-9/12 mx-auto mt-20">
+            <form>
+                <div className="relative z-0 w-full mb-6 group">
+                    <input
+                        value={user.name}
+                        onChange={(event) => setUser({ ...user, name: event.target.value })}
+                        type="text" name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Your Name</label>
+                </div>
+                <div className="relative z-0 w-full mb-6 group">
+                    <input
+                        value={user.email}
+                        onChange={(event) => setUser({ ...user, email: event.target.value })}
+                        type="email" name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
+                </div>
+                <div className="relative z-0 w-full mb-6 group">
+                    <input
+                        value={user.password}
+                        onChange={(event) => setUser({ ...user, password: event.target.value })}
+                        type="password" name="floating_password" id="floating_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <label htmlFor="floating_password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
+                </div>
+                <div className="w-fit hover:underline mb-5">
+                    <Link href='/signUp'><p>New to this site?</p></Link>
+                </div>
+                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+            </form>
+        </div>
+    )
 }
